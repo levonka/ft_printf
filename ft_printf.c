73e90402740b99dfc49aad8f36153f93984892c8
Levonka <levonkana@gmail.com>
2019-01-24 07:38:56 +0300

    test commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: agottlie <agottlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 15:35:03 by agottlie          #+#    #+#             */
-/*   Updated: 2019/01/24 07:30:53 by agottlie         ###   ########.fr       */
+/*   Updated: 2019/01/24 07:38:20 by agottlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		main(void)
 	// %f, %lf,  %Lf, %%");
 	ft_printf("'%-014.15s', %+10.4s\n", "helloworld", "levonka");
 	printf("'%-014.15s', %+10.4s\n", "helloworld", "levonka");
-	printf("HI\n");
+	printf("HI Sergey\n");
 	// printf("%05.5s");
 	// ft_printf("%05.1lld");
 	// ft_printf("how are u doing? %ld\n", LONG_MAX + 1);

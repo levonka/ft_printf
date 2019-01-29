9c7611017422741cc365eb92a6b898d4a724a900
Levonka <levonkana@gmail.com>
2019-01-29 09:57:40 +0300

    optimised printfunctions.c(new ft 'cmp')

--- print_functions.c
+++ print_functions.c
@@ -1,11 +1,19 @@
 #include "ft_printf.h"
 
+int		cmp(char *type, char *c)
+{
+	return ((ft_strcmp(type, c) == 0) ? 1 : 0);
+}
+
 void	ft_print_dispatcher(t_type *node, va_list args)
 {
-	if (ft_strcmp(node->type, "s") == 0)
+	char	*t;
+
+	t = node->type;
+	if (cmp(t, "s"))
 		ft_print_string(node, ft_strdup(va_arg(args, char *)), 0);
-	else if (ft_strcmp(node->type, "d") == 0 || ft_strcmp(node->type, "i") == 0)
+	else if (cmp(t, "d") || cmp(t, "i"))
 		ft_print_int(node, ft_itoa(va_arg(args, int)), 0);
-	else if (ft_strcmp(node->type, "c") == 0)
+	else if (cmp(t, "c"))
 		ft_print_char(node, va_arg(args, int), 0);
 }

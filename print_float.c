dc18a2fb6aecd8617790656957789b94aa97d192
Levonka <levonkana@gmail.com>
2019-02-17 15:30:11 +0300

    fails 25632

--- print_float.c
+++ print_float.c
@@ -6,7 +6,7 @@
 /*   By: agottlie <agottlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 17:36:54 by agottlie          #+#    #+#             */
-/*   Updated: 2019/02/17 14:03:48 by agottlie         ###   ########.fr       */
+/*   Updated: 2019/02/17 15:28:10 by agottlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -202,6 +202,7 @@ int			ft_print_float(t_type *node, double n, int i)
 
  	str = ft_ftoa(n, (((node->precision) == -1) ? 6 : node->precision), node);
 	// printf(">>>'%s'\n", str);
+ 	// printf("%f\n", n);
 	if (is_const(str) == SUCCESS)
 	{
 		len = ft_print_fconst(node, str, i);
@@ -212,23 +213,24 @@ int			ft_print_float(t_type *node, double n, int i)
 	// if (cmp(str, "0") && (node->precision == 0))
 	// 	str[0] = '\0';
 	minus = ft_isnegative(str);
+ 	// printf("<><><'%s'\n", str);
 	if ((node->width != -1 && node->width >= (int)ft_strlen(str) + 1))
 		// (node->precision != -1 && node->precision >= (int)ft_strlen(str) + 1))
 	{
-		node->zero = (cmp(str, "0") == 1);
+		node->zero = (cmp(str, "0") == 1 || cmp(str, "0.") == 1);
 		len = ((node->width > (int)ft_strlen(str)) ? node->width : ft_strlen(str));
 		str2 = ft_strnew(len + 3);
 		ft_zerofiller(node, str2, len, &i);
 		ft_zerofiller3(node, str2, len, 0);
 		ft_fillin_num(node, str2, str, len);
-		// printf("<<'%s'\n", str2);
 		(node->precision < node->width && ft_isfl_in(node, '-') == -1) ? turnoff_fl(node->flags, ' ') : 0;
 		(ft_isfl_in(node, ' ') == 0) ? ft_flagsp_num(str2, len) : 0;
-		if (minus == 0 && str2[0] == '0')
+		// ft_printf("|%0+10.f|", -0.);
+		if ((minus == 0 && str2[0] == '0' && node->zero == 0) || (ft_isfl_in(node, '+') == 0 && ft_isfl_in(node, '0') == 0))
 			str2[0] = '-';
 		else if (minus == 0)
 			ft_flagminus_num(node, str2, len);
-		// printf("|%08.2f|", -999.999);
+		// printf("<<'%s'\n", str2);
 		ft_flagplus_num(node, str2, minus, len);
 		len = (node->width > node->precision) ? node->width : node->precision;
 		if (len < (int)ft_strlen(str2) && str2[ft_strlen(str2) - 1] == ' ')
